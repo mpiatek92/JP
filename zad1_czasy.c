eb3b721b9e46ebb04fe747ed547c6714f186a51c
mpiatek92 <mpiatek@inf.ug.edu.pl>
2014-01-14 20:23:21 +0100

    Update zad1_czasy.c
    Ostateczna wersja

--- zad1_czasy.c
+++ zad1_czasy.c
@@ -1,18 +1,21 @@
 #include<stdio.h>
 #include<stdlib.h>
+#define DOBA 86399 /* sekund */
 struct czas
 {
 	int godzina;
 	int minuta;
 	int sekunda;
 };
-void print_czas(struct czas c1)
+typedef struct czas czas;
+
+void print_czas(czas c)
 {
-	printf("%02d:%02d:%02d",c1.godzina,c1.minuta,c1.sekunda);
+	printf("%02d:%02d:%02d",c.godzina,c.minuta,c.sekunda);
 }
-struct czas get_czas_from_s(int s)
+czas get_czas_from_s(int s)
 {
-	struct czas wynik;
+	czas wynik;
 	div_t d = div(s,60*60);
 	wynik.godzina = d.quot;
 	d = div(d.rem,60);
@@ -20,42 +23,82 @@ struct czas get_czas_from_s(int s)
 	wynik.sekunda = d.rem;
 	return wynik;
 }
-int get_s_from_czas(struct czas c)
+int get_s_from_czas(czas c)
 {
 	return c.godzina*60*60 + c.minuta*60+c.sekunda;
 }
-struct czas diff(struct czas c1,struct czas c2)
+czas diff(czas c1, czas c2)
 {
 	return get_czas_from_s(abs(get_s_from_czas(c1)-get_s_from_czas(c2)));
 }
+void test(void)
+{
+		int vals[] = {0,0,0,0,0,0,0,0,0,0};
+		czas test;
+		int i;
+		for(i = 0; i <= DOBA; i++)
+		{
+			test = get_czas_from_s(i);
+			vals[(test.godzina % 10)]++;
+			vals[(test.godzina / 10)]++;
+
+			vals[(test.minuta % 10)]++;
+			vals[(test.minuta / 10)]++;
+
+			vals[(test.sekunda % 10)]++;
+			vals[(test.sekunda / 10)]++;
+		}
+		int max, maxpos;
+		max = vals[0];
+		maxpos = 0;
+		printf("Wystapienia poszczegolnych cyfr na wyswietlaczu od 00:00:00 do 23:59:59:\n");
+		for(i = 0; i < 10; i++)
+		{
+			printf("%d: %d\n", i, vals[i]);
+			if(vals[i] > max)
+			{
+				max = vals[i];
+				maxpos = i;
+			}
+		}
+		printf("\nCyfra/y najczesciej wystepujaca/e to %d", (maxpos));
+		for(i = 0; i < 10; i++)
+		{
+			if(i!= maxpos && vals[i] == max)
+			{
+				printf(" i %d", i);
+			}
+		}
+		printf("\n");
+}
 int main(int argc, char** argv)
 {
+	test();
 	int h,m,s;
-	struct czas c1,c2;
-	printf("Podaj -1 zeby wyjsc\n");
+	czas c1,c2;
+	printf("Wpisz -1 aby zakonczyc\n");
 	while(1)
 	{
 		printf("Podaj 1. czas w formacie HH:MM:SS\n");
 		scanf("%d:%d:%d", &h,&m,&s);
+		if(h == -1)
+		{
+			break;
+		}
 		c1.godzina = h;
 		c1.minuta = m;
 		c1.sekunda = s;
-		if(c1.godzina == -1)
+		printf("\nPodaj 2. czas w formacie HH:MM:SS\n");
+		scanf("%d:%d:%d", &h,&m,&s);
+		if(h == -1)
 		{
 			break;
 		}
-		printf("\nPodaj 2. czas w formacie HH:MM:SS\n");
-		scanf("%d:%d:%d", &h,&m,&s);
 		c2.godzina = h;
 		c2.minuta = m;
 		c2.sekunda = s;
-		if(c2.godzina == -1)
-		{
-			break;
-		}
 		printf("\nWynik to: ");
 		print_czas(diff(c1,c2));
 		printf("\n----\n");
 	}
 }
-
